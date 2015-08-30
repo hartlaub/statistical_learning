bc40e6544cb4c6041bb4583c340cc7250fe6789a
John Zito <johnczito@gmail.com>
2015-08-29 21:00:47 -0400

    Delete tests

--- newtest.R
+++
@@ -1,5 +0,0 @@
-mydata <- data.frame(X = rnorm(100), Y = rnorm(100))
-
-sum(mydata$X)
-
-sum(mydata$Y)
--- test.R
+++
@@ -1,5 +0,0 @@
-mydata <- data.frame(X = rnorm(200), Y = rnorm(200))
-
-mymodel <- lm(Y ~ X, data = mydata)
-
-summary(mymodel)

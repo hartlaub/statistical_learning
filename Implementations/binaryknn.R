70e970e12b7186f2552b5a7b0fd0dd22c24704fb
John Zito <johnczito@gmail.com>
2015-09-18 13:36:44 -0400

    update

--- Implementations/binaryknn.R
+++ Implementations/binaryknn.R
@@ -2,7 +2,75 @@
 
 #  A homemade implementation -------------------------------------
 
+classify <- function(x, df, k) {
+  # k-nearest neighbors binary classification of
+  # the input vector x according to training data
+  # in df. 
+  #
+  # Args:
+  #   x: a numeric vector of inputs
+  #   df: a data frame of training data with a
+  #       binary classification in the first column
+  #       and input data in the remaining columns
+  #   k: the number of neighbors averaged over
+  #
+  # Returns:
+  #   1 if knn average > 0.5
+  #   0 if knn average <= 0.5
+  
+  df$new <- apply(df[, 2:ncol(df)], 1, distance, b = x)
+  df <- df[order(df$new), ]
+  df <- df[1:k, ]
+  
+  z <- mean(df[, 1])
+  
+  return(as.numeric(z > 0.5))
+  
+}
 
+distance <- function(a, b) {
+  # Computes the euclidean distance
+  # between numeric vectors a and b
+  # of equal length
+  #
+  # Args:
+  #   a: numeric vector
+  #   b: numeric vector
+  #
+  # Returns:
+  #   ||a - b||
+  
+  return(sqrt(sum((a - b) ^ 2)))
+  
+}
+
+KNN <- function(tr, te, k) {
+  # Binary classification of a test
+  # data set by k-nearest-neighbors averaging
+  #
+  # Args:
+  #   tr: a data frame of training data with a 
+  #       binary classifier in the first column
+  #       and numeric input data in the rest.
+  #   te: a data frame of numeric test data.
+  #       Assumes ncol(te) = ncol(tr) - 1
+  #    k: the number of neighbors averaged over
+  #
+  # Returns:
+  #   te with a column of binary classifiers
+  #   added to it
+  
+  te$class <- apply(te, 1, classify, df = tr, k = k)
+  
+  return(te)
+  
+}
 
 #  knn {class} ---------------------------------------------------
+
+library(class)
+
+?knn()
+
+#  Comparison ----------------------------------------------------
 
--- Implementations/pcr.R
+++ Implementations/pcr.R
@@ -1,1 +1,43 @@
 #  Principal Component Regression
+
+#  Homemade implementation ---------------------------------------
+
+PCR <- function(Y, X, M) {
+  
+  ybar <- rep(mean(Y), length(Y))
+  
+  V <- svd(X)$v
+  Z <- X %*% V
+  
+  model <- lm(I(Y - ybar) ~ 0 + Z[, 1:M])
+  
+  return(model)
+  
+}
+
+#  pcr {pls} -----------------------------------------------------
+  
+library(pls)
+
+?pcr()
+
+#  Comparison ----------------------------------------------------
+
+X <- data.frame(matrix(c(rnorm(1000, 4, 5), 
+                         rnorm(1000),
+                         rnorm(1000, 4, 500),
+                         runif(1000, -100, 4),
+                         rnorm(1000, 1000, 10)),
+                         nrow = 1000, 
+                         ncol = 5))
+
+pcr.fit <- pcr(X1 ~ ., data = X, scale = TRUE, validation = "CV")
+summary(pcr.fit)
+  
+  
+  
+  
+  
+  
+  
+  

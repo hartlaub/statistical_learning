0b602a181549857e6165e392184c1e330a0c5f02
ROBIN DUNN <robin.dunn1012@gmail.com>
2015-09-18 13:56:27 -0400

    Exercises from ESL and ISLR

---
+++ Exercises/ISLR 6-9.R
@@ -0,0 +1,139 @@
+### This code satisfies the requirements Problem 2.9 from "Introduction to 
+### Statistical Learning with Applications in R."
+### Code for ridge regression, lasso, PCR, and PLS based off of code from
+### Section 6.5 from ISLR.
+### 9/16/15
+
+require(ISLR) # contains College dataset
+require(glmnet) # for ridge regression and lasso
+require(pls) # for PCR and PLS
+
+CollegeCopy <- College
+
+### Coerce Private == "Yes" to Private = 1
+### and Private == "No" to Private = 0.
+
+CollegeCopy$Private <- as.numeric(CollegeCopy$Private) - 1
+
+### a) Randomly split the data set into a training set and a test set.
+
+set.seed(100)
+random <- sample(nrow(CollegeCopy), nrow(CollegeCopy))
+
+training <- CollegeCopy[random[1:389],]
+test <- CollegeCopy[random[390:nrow(CollegeCopy)],]
+
+### b) Fit a linear model using least squares on the training set,
+###    and report the test error obtained.
+
+# Fit the linear model.
+
+linearModel <- lm(Apps ~ ., data = training)
+summary(linearModel)
+
+# Calculate test error.
+
+testPredictions <- as.numeric(predict(linearModel, test))
+testResids <- test$Apps - testPredictions
+mean(testResids^2) # MSE = 973540.9
+
+### c) Fit a ridge regression model on the training set, with lambda chosen
+###    by cross-validation. Report the test error obtained.
+
+# Create x and y matrices.
+
+x <- model.matrix(Apps ~ ., training)
+y <- training$Apps
+
+# Perform ten-fold cross validation. 
+# bestlam stores the choice of lambda that minimizes MSE.
+
+set.seed(1)
+cv.out <- cv.glmnet(x, y, alpha = 0, nfolds = 10)
+plot(cv.out) # Cross-validation curve with upper and lower standard dev curves.
+             # One line is at log(Lambda) at which MSE is minimized (cv.out$lambda.min)
+             # One line is at log(Lambda) corresponding to largest value of lambda
+             # within 1 standard error of mininum. (cv.out$lambda1se)
+
+bestlam <- cv.out$lambda.min
+bestlam
+
+# Fit ridge regression model and view coefficients.
+
+ridge.mod <- glmnet(x, y, alpha = 0, lambda = bestlam)
+predict(ridge.mod, type = "coefficients")
+
+# Calculate test error. 
+
+testx <- model.matrix(Apps ~ ., data = test)
+ridge.pred <- predict(ridge.mod, testx)
+mean((ridge.pred - test$Apps)^2) # MSE = 860927.1
+
+### d) Fit a lasso model on the training set, with lambda chosen by
+###    cross-validation. Report the test error obtained, along with the number
+###    of non-zero coefficient estimates.
+
+# Create x and y matrices.
+
+x <- model.matrix(Apps ~ ., training)
+y <- training$Apps
+
+# Perform ten-fold cross validation. 
+# bestlam stores the choice of lambda that minimizes MSE.
+
+set.seed(1)
+cv.out <- cv.glmnet(x, y, alpha = 1, nfolds = 10)
+plot(cv.out) # Cross-validation curve with upper and lower standard dev curves.
+             # One line is at log(Lambda) at which MSE is minimized (cv.out$lambda.min)
+             # One line is at log(Lambda) corresponding to largest value of lambda
+             # within 1 standard error of mininum. (cv.out$lambda1se)
+
+bestlam <- cv.out$lambda.min
+bestlam
+
+# Fit lasso model and view coefficients.
+
+lasso.mod <- glmnet(x, y, alpha = 1, lambda = bestlam)
+predict(lasso.mod, type = "coefficients") # All 18 coefs are non-zero
+
+# Calculate test error. 
+
+testx <- model.matrix(Apps ~ ., data = test)
+lasso.pred <- predict(lasso.mod, testx)
+mean((lasso.pred - test$Apps)^2) # MSE = 1368719
+
+### e) Fit a PCR model on the training set, with M chosen by cross-validation.
+###    Report the test error obtained, along with the value of M selected by
+###    cross-validation.
+
+# Fit PCR model and look for cross-validated value of M that minimizes
+# root MSE (from summary) or MSE (from plot).
+# This sugguests M = 17.
+
+set.seed(1)
+pcr.fit <- pcr(Apps ~ ., data = training, scale = TRUE, validation = "CV")
+summary(pcr.fit)
+validationplot(pcr.fit, val.type = "MSEP")
+
+# Calculate test error.
+
+pcr.pred <- predict(pcr.fit, test, ncomp = 17)
+mean((pcr.pred - test$Apps)^2) # MSE = 1359059
+
+### f) Fit a PLS model on the training set, with M chosen by cross-validation.
+###    Report the test error obtained, along with the value of M selected by
+###    cross-validation.
+
+# Fit PLS model and look for cross-validated value of M that minimizes
+# root MSE (from summary) or MSE (from plot).
+# This sugguests M = 8.
+
+set.seed(1)
+pls.fit <- plsr(Apps ~ ., data = training, scale = TRUE, validation = "CV")
+summary(pls.fit)
+validationplot(pls.fit, val.type = "MSEP")
+
+# Calculate test error.
+
+pls.pred <- predict(pls.fit, test, ncomp = 8)
+mean((pls.pred - test$Apps)^2) # MSE = 1372594
---
+++ Exercises/ex2_8.R
@@ -0,0 +1,273 @@
+### Elements of Statistical Learning: Exercise 2.8
+### Implementation of K-nearest neighbors binary classification
+### and OLS regression binary classification.
+
+#############################################################
+#################### FUNCTIONS ##############################
+#############################################################
+
+### Function: dist (K nearest neighbors)
+### 
+### Inputs:
+###    y and x: Two numerical vectors of same length
+###
+### Output:
+###    Euclidean distance between y and x.
+
+dist <- function(y, x){
+  return(sqrt(sum((y - x )^2)))
+}
+
+### Function: classify (K nearest neighbors)
+### 
+### Inputs: 
+###   te_row: A row of the test data. 
+###   tr: Complete training dataset. The 0/1 classifications should be 
+###       stored in the first column of tr.
+###   k: Number of nearest neighbors that user wishes to use in order to
+###      classify te_row.
+###   Uses dist() function.
+###
+### Output:
+###   Outputs 0 if majority of k rows of tr that have closest Euclidean distance 
+###   to te_row are classified as 0.
+###   Outputs 1 if majority of k rows of tr that have closest Euclidean distance 
+###   to te_row are classified as 1.
+
+classify <- function(te_row, tr, k){
+  
+  ### tr$new1 computes Euclidean distance between te_row (a row of the test data) 
+  ### and each of the rows of tr (training data).
+  tr$new1 <- apply(tr[,2:ncol(tr)], 1, dist, x = te_row[2:length(te_row)])
+  
+  ### Order tr (training data) from smallest to largest Euclidean distance 
+  ### from te_row.
+  tr <- tr[order(tr$new1), ]
+  
+  ### Extract k rows of tr that are closest to te_row.
+  tr <- tr[1:k, ]
+  
+  ### Take mean of 0/1 classification of k closest neighbors in training data.
+  z <- mean(tr[,1])
+  
+  ### If z > 0.5, more than half of k nearest neighbors are classified as 1,
+  ### so return 1.
+  ### If z <= 0.5, more than half of k nearest neighbors are classified as 0,
+  ### so return 0.
+  return(as.numeric(z > 0.5))
+}
+
+### Function: Knn (K nearest neighbors)
+###
+### Inputs:
+###   tr: Complete training dataset. The 0/1 classifications should be 
+###       stored in the first column of tr.
+###   te: Complete test dataset. Should not contains classifications of
+###       test data.
+###   k: Number of nearest neighbors that user wishes to use in order to
+###      classify te_row.
+###   Uses classify() function.
+###
+### Outputs:
+###   Returns test dataset te with additional column 'classify' that contains 
+###   0/1 classification of all rows of te.
+
+Knn <- function(tr, te, k){
+  te$classify <- apply(te, 1, classify, tr = tr, k = k)
+  return(te)
+}
+
+### Function: linearClassification (OLS regression)
+###
+### Inputs:
+###   tr: Training data. First row ('V1') should contain classification of
+###       training data.
+###   te: Test data. First row should not contain classification of test data.
+###
+### Output: 
+###   Outputs test data with a 'classify' column, which contains the 0/1
+###   classification of each test observation.
+
+linearClassification <- function(tr, te){
+  
+  ### linearModel stores linear regression model created from training data.
+  colnames(tr)[1] <- "trainingClass"
+  linearModel <- lm(trainingClass ~ .- trainingClass, data = tr)
+  
+  ### Use linearModel to predict probability that test observations are
+  ### truly classified as 1.
+  test_predictions <- predict(linearModel, te)
+  test_classification <- test_predictions
+  
+  ### If Pr(test_classification == 1) > 0.5, classify that test observation as 1.
+  ### If Pr(test_classification == 1) <= 0.5, classify that test observation as 0.
+  test_classification[test_classification > 0.5] <- 1
+  test_classification[test_classification <= 0.5] <- 0
+  
+  ### 'classify' column of te will contain classification of all observations
+  ### in test dataset.
+  te$classify <- test_classification
+  
+  return(te)
+}
+
+#############################################################
+######################## MAIN ###############################
+#############################################################
+
+setwd("C:/Users/Robin Dunn/Documents/Robin 7th Semester/Statistical Learning/statistical_learning/Exercises/Data")
+
+### training stores training data. 
+training <- read.table("zip.train.csv", sep = " ")
+training$V258 <- NULL
+
+### test stores test data.
+test <- read.table("zip.test.csv", sep = " ")
+
+### Only use training and test data rows with true classification equal to 
+### 2 or 3.
+training <- training[training$V1==2 | training$V1==3, ]
+test <- test[test$V1==2 | test$V1==3, ]
+
+### In training and test data, if classification is 2, set classification to 0.
+### If classification is 3, set classification to 1.
+training$V1 <- training$V1 - 2
+test$V1 <- test$V1 - 2
+
+###########################
+### k nearest neighbors ###
+###########################
+
+#############
+### k = 1 ### 97.53 % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 1.
+test_new <- Knn(training, test, 1)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 1.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=1)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+#############
+### k = 3 ### 96.98 % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 3.
+test_new <- Knn(training, test, 3)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 3.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=3)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+#############
+### k = 5 ### 96.98 % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 5.
+test_new <- Knn(training, test, 5)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 5.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=5)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+#############
+### k = 6 ###  % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 6.
+test_new <- Knn(training, test, 6)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 6.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=6)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+#############
+### k = 7 ### 96.70 % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 7.
+test_new <- Knn(training, test, 7)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 7.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=7)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+#############
+### k = 15 ## 96.15 % accuracy
+#############
+
+### test_new stores test data with 0/1 classification of test data
+### in 'classify' column, using my Knn function with k = 15.
+test_new <- Knn(training, test, 15)
+
+### Outputs percentage of correctly classified test data.
+sum(test_new$classify == test[,1]) / length(test_new$classify)
+
+### x stores vector with 0/1 classification of test data
+### using R's knn function with k = 15.
+require(class)
+x <- knn(training[,2:ncol(training)], test[,2:ncol(test)], training[,1], k=15)
+sum(x == test[,1]) / length(x)
+
+### Compares classifcations in x (from R's knn function) to
+### classifications in test_new$classify (from my Knn function)
+table(x == test_new$classify)
+
+
+###########################
+###### OLS regression ##### 95.88 % accuracy
+###########################
+
+test_new <- linearClassification(training, test[,2:length(test)])
+sum(test_new$classify == test[,1]) / length(test_new$classify)
